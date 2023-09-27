451f62c6875f260634d3f630a83b8397ac2065e5
Octavio Sebastian Ballon Copaira <00001225389173sp@aluno.educacao.sp.gov.br>
2023-09-27 12:12:35 -0300

    backend

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,10 @@ const app =  express();
 const port = 3000;
 
 const jogo = require('./controllers/jogoController.js');
-const jogador = require('./controllers/jogoController.js');
+const jogador = require('./controllers/jogadorControler.js');
 
 app.use(bodyParser.json());
 app.use(cors())
 app.use('/jogo', jogo);
+app.use('/jogador', jogador);
 app.listen(port, () => console.log(`Servidor rodando porta ${port}!`))
