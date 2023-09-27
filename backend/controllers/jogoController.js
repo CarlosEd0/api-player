9e879a0b3b2862f636e8b56c638ac7e22f2e6063
Carlos Eduardo Araujo Da Silva <00001094539399sp@aluno.educacao.sp.gov.br>
2023-09-27 11:41:35 -0300

    Atualizações do back

--- backend/controllers/jogoController.js
+++ backend/controllers/jogoController.js
@@ -1,9 +1,16 @@
 const express = require ('expess');
 const router = express.Router();
-const Jogos = require('../models').Jogos;
+const Jogos = require('../models').Jogo;
 
+//Cadastro de Jogo
 router.post('/', async (req, res) => {
     const {descricao} = req.body;
-    const newEdit = await Jogos.create ({descricao})
+    const {titulo} = req.body;
+    const {imagensJogo} = req.body;
+    const {desenvolvedoraJogo} = req.body;
+    const {dataLancamento} = req.body;
+    const newEdit = await Jogos.create ({descricao, titulo, imagensJogo, desenvolvedoraJogo, dataLancamento})
     res.status(200).json({message: 'Cadastrado com sucesso'});
 });
+
+module.exports=router;
