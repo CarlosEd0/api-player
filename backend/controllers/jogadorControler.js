62a2a272572372f2fd20c9f2fd5a3474f8a39f1c
Carlos Eduardo Araujo Da Silva <00001094539399sp@aluno.educacao.sp.gov.br>
2023-09-27 11:48:41 -0300

    atualizações

--- backend/controllers/jogadorControler.js
+++ backend/controllers/jogadorControler.js
@@ -6,9 +6,7 @@ router.post('/', async (req, res) => {
     const {nome} = req.body;
     const {email} = req.body;
     const {data_de_registro} = req.body;
-    const {createdAt} = req.body;
-    const {updatedAt} = req.body;
-    const newEdit = await Categoria.create({descricao})
+    const newEdit = await Categoria.create({nome, email, data_de_registro})
     res.status(200).json({message: 'Cadastro com suscesso'});
 });    
 module.exports=router;
--- backend/controllers/jogoController.js
+++ backend/controllers/jogoController.js
@@ -1,4 +1,4 @@
-const express = require ('expess');
+const express = require ('express');
 const router = express.Router();
 const Jogos = require('../models').Jogo;
 
@@ -9,7 +9,7 @@ router.post('/', async (req, res) => {
     const {imagensJogo} = req.body;
     const {desenvolvedoraJogo} = req.body;
     const {dataLancamento} = req.body;
-    const newEdit = await Jogos.create ({descricao, titulo, imagensJogo, desenvolvedoraJogo, dataLancamento})
+    const newEdit = await Jogos.create ({descricao, titulo, imagensJogo, desenvolvedoraJogo, dataLancamento});
     res.status(200).json({message: 'Cadastrado com sucesso'});
 });
 
