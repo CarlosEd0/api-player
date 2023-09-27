b147fa15ad640cfee2ae64918a780eba257e6654
Octavio Sebastian Ballon Copaira <00001225389173sp@aluno.educacao.sp.gov.br>
2023-09-27 11:40:00 -0300

    backend

--- backend/controllers/jogadorControler.js
+++ backend/controllers/jogadorControler.js
@@ -2,3 +2,13 @@ const express = require ('expess');
 const router = express.Router();
 const Jogador = require('../models').Jogador;
 
+router.post('/', async (req, res) => {
+    const {nome} = req.body;
+    const {email} = req.body;
+    const {data_de_registro} = req.body;
+    const {createdAt} = req.body;
+    const {updatedAt} = req.body;
+    const newEdit = await Categoria.create({descricao})
+    res.status(200).json({message: 'Cadastro com suscesso'});
+});    
+module.exports=router;
