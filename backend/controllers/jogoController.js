c4feef05eb51307855ba7404f0cb0f9fe90de9e9
Carlos Eduardo Araujo Da Silva <00001094539399sp@aluno.educacao.sp.gov.br>
2023-09-28 11:56:15 -0300

    28/09 atualizado

--- backend/controllers/jogoController.js
+++ backend/controllers/jogoController.js
@@ -10,7 +10,7 @@ router.post('/add', async (req, res) => {
     const {desenvolvedoraJogo} = req.body;
     const {dataLancamento} = req.body;
     const newEdit = await Jogos.create ({descricao, titulo, imagensJogo, desenvolvedoraJogo, dataLancamento});
-    res.status(200).json({message: 'Cadastrado com sucesso'});
+    res.status(200).json({message: 'Cadastrado com sucesso', newEdit});
 });
 
 module.exports=router;
