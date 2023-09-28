d1a4aedbdab0525a924ce1df010c398c05bf7782
Octavio Sebastian Ballon Copaira <00001225389173sp@aluno.educacao.sp.gov.br>
2023-09-28 11:25:28 -0300

    teste de api cadastro de jogador OK

--- backend/controllers/jogadorControler.js
+++ backend/controllers/jogadorControler.js
@@ -2,11 +2,14 @@ const express = require ('express');
 const router = express.Router();
 const Jogador = require('../models').Jogador;
 
-router.post('/', async (req, res) => {
+router.post('/add', async (req, res) => {
     const {nome} = req.body;
     const {email} = req.body;
     const {data_de_registro} = req.body;
-    const newEdit = await Categoria.create({nome, email, data_de_registro})
-    res.status(200).json({message: 'Cadastro com suscesso'});
-});    
+    const newEdit = await Jogador.create({nome, email, data_de_registro})
+    res.status(200).json({message: 'Cadastro com suscesso', newEdit});
+}); 
+
+
+// Fim do Controller Jogador 
 module.exports=router;
