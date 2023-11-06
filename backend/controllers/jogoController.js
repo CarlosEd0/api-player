f9482f329f3dd18968b94ad49884ba2f2455af10
Carlos Eduardo Araujo Da Silva <00001094539399sp@aluno.educacao.sp.gov.br>
2023-11-06 10:56:50 -0300

    Controllers

--- backend/controllers/jogoController.js
+++ backend/controllers/jogoController.js
@@ -13,4 +13,37 @@ router.post('/add', async (req, res) => {
     res.status(200).json({message: 'Cadastrado com sucesso', newEdit});
 });
 
+// Buscar todos os jogos 
+router.get('/all', async (req, res) => {
+    const jogos = await Jogos.findAll();
+    res.status(200).json(jogos);
+});
+
+//Altera Jogo por ID (PUT)
+router.put('/:id', async (req, res) => {
+    const {descricao} = req.body;
+    const {titulo} = req.body;
+    const {imagensJogo} = req.body;
+    const {desenvolvedoraJogo} = req.body;
+    const {dataLancamento} = req.body;
+
+    await Jogos.update(
+        { descricao, titulo, imagensJogo, desenvolvedoraJogo, dataLancamento},
+        {
+            where: {id:req.params.id},
+        }
+        
+    );
+    res.status(200).json({message: 'Atualizado com sucesso'});
+});
+
+router.delete('/:id', async (req, res) =>{
+    await Jogos.destroy({
+        where:{
+            id: req.params.id,
+        },
+    });
+    res.status(200).json({message: 'Excluído com sucesso'})
+});
+
 module.exports=router;
